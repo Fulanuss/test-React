8d65eb920ce245aa2c3d18b97b0c9fadb174ba23
Abdou KA <hayranke@gmail.com>
2023-05-27 10:02:02 +0000

    commitReact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Form>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Entrez votre email" />
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Mot de passe" />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      <Form.Check type="checkbox" label="Cochez ici" />
+    </Form.Group>
+    <Button variant="primary" type="submit">
+      Soumettre
+    </Button>
+  </Form>
+);
 }
 
 export default App;
